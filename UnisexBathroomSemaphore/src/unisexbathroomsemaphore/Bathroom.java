cccdc9df2a339aadbe5150225b814fe70c934741
Breno Viana <brenomfviana@gmail.com>
2017-05-25 22:38:38 -0300

    Starts the implementation of unisex bathroom semaphore.

--- UnisexBathroomSemaphore/src/unisexbathroomsemaphore/Bathroom.java
+++ UnisexBathroomSemaphore/src/unisexbathroomsemaphore/Bathroom.java
@@ -3,12 +3,77 @@
  */
 package unisexbathroomsemaphore;
 
+import java.util.List;
+import unisexbathroomsemaphore.person.Person;
+
 /**
- * .
+ * This class respresents the bathroom.
  *
  * @author Breno Viana
  * @version 25/05/2017
  */
 public class Bathroom {
-    
+
+    // Time of use available (seconds)
+    private static int TIME_OF_USE_AVAILABLE = 10;
+
+    // Singleton
+    private static Bathroom instance = new Bathroom(10);
+
+    // Bathroom capacity
+    private final int capacity;
+    // Users list
+    private List<Person> users;
+
+    /**
+     * Constructor.
+     *
+     * @param capacity Bathroom capacity
+     */
+    public Bathroom(int capacity) {
+        this.capacity = capacity;
+    }
+
+    /**
+     * Get instance of bathroom.
+     *
+     * @return Instance of bathroom
+     */
+    public Bathroom getInstance() {
+        return instance;
+    }
+
+    /**
+     * Add a user in the bathroom.
+     *
+     * @param person A person
+     */
+    public void addUser(Person person) {
+        // 
+    }
+
+    /**
+     * Remove a user from the bathroom.
+     *
+     * @param person A person
+     */
+    public void removeUser(Person person) {
+        // 
+    }
+
+    /**
+     * .
+     * @return
+     */
+    public boolean isFull() {
+        return this.capacity == this.users.size();
+    }
+
+    /**
+     * .
+     * @return
+     */
+    public boolean isEmpty() {
+        return this.users.isEmpty();
+    }
 }
--- UnisexBathroomSemaphore/src/unisexbathroomsemaphore/person/Person.java
+++ UnisexBathroomSemaphore/src/unisexbathroomsemaphore/person/Person.java
@@ -11,8 +11,26 @@ package unisexbathroomsemaphore.person;
  */
 public class Person implements Runnable {
 
+    // Bathroom use time
+    protected int bathroomUseTime;
+
+    /**
+     * .
+     * @param bathroomUseTime
+     */
+    public Person(int bathroomUseTime) {
+        this.bathroomUseTime = bathroomUseTime;
+    }
+
     @Override
     public void run() {
         // TODO
+    }
+
+    /**
+     * .
+     */
+    public void useBathroom() {
+        //
     }
 }
